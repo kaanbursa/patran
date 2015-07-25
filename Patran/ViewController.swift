8c664d285fce4929833c080df64b14d284879375
Kaan Bursa <kaanbursa9@gmail.com>
2015-07-25 14:19:57 +0300

    Initial Commit

---
+++ Patran/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Patran
+//
+//  Created by kaanbursa on 7/25/15.
+//  Copyright (c) 2015 kaanbursa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
